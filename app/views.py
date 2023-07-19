14dd2b50f67d0158ecbbd37b1ed1d2d4a71a5ebe
sravanivallepu <sravanivallepu00294@gmail.com>
2023-07-20 00:20:30 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+from app.forms import *
+def registration(request):
+    d={'usfo':UserForm(),'pfo':ProfileForm()}
+    if request.method=='POST' and request.FILES:
+        ufd=UserForm(request.POST)
+        pfd=ProfileForm(request.POST,request.FILES)
+        if ufd.is_valid() and pfd.is_valid():
+            NSUO=ufd.save(commit=False)
+            password=ufd.cleaned_data['password']
+            NSUO.set_password(password)
+            NSUO.save()
+
+            NSPO=pfd.save(commit=False)
+            NSPO.username=NSUO
+            NSPO.save()
+            return HttpResponse('Regsitration is Susssessfull')
+        else:
+            return HttpResponse('Not valid')
+    return render(request,'registration.html',d)
--- project37/settings.py
+++ project37/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'project37.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
